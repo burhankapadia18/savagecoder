909a2abe82b4242271aa94646fc35ce3e4969fe0
burhankapadia18 <www.burhan18@gmail.com>
2021-08-03 12:55:46 +0530

    added login, signup functionality

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('contact/', views.contact, name='contact')
+    path('contact/', views.contact, name='contact'),
+    path('search/', views.search, name='search'),
+    path('signup/', views.signUp, name='signUp')
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from home.models import Contact
+from blog.models import Post
+from django.contrib.auth.models import User
 from django.contrib import messages
 
 # Create your views here.
@@ -25,3 +27,44 @@ def contact(request):
             contact.save()
             messages.success(request, 'Your query has been sent successfully')
     return render(request, 'home/contact.html')
+
+def search(request):
+    query = request.GET['query']
+    if len(query)<2 or len(query)>70:
+        messages.warning(request, 'Please enter valid keyword')
+        return render(request,'home/home.html')
+    allPost1 = Post.objects.filter(title__icontains=query)
+    allPost2 = Post.objects.filter(content__icontains=query)
+    allPost = allPost1.union(allPost2)
+    if len(allPost) == 0:
+        messages.warning(request, 'sorry! there are no blogs related to your search')
+        return redirect('home')
+    params = {'allPost': allPost, 'query': query}
+    messages.success(request, f'found {len(allPost)} blog(s) related to your search')
+    return render(request, 'home/search.html', params)
+
+
+def signUp(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+
+        if fname=="" or lname=="" or username=="":
+            messages.warning(request, 'Please fill proper details')
+            return render(request,'home/signup.html')
+        if pass1!=pass2:
+            messages.error(request, "Password doesn't match")
+            return render(request,'home/signup.html')
+        
+        myuser = User.objects.create_user(username,email,pass1)
+        myuser.first_name = fname
+        myuser.last_name = lname
+        myuser.save()
+        messages.success(request, 'Your SavageCoder account has been created successfully!')
+        return redirect('home')
+    else:   
+        return render(request,'home/signup.html')
