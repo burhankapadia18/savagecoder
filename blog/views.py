1ca4f9713031e3df4e094353c06ed987a40d73ce
burhankapadia18 <www.burhan18@gmail.com>
2021-09-22 14:48:48 +0530

    minor bug fixed

--- blog/views.py
+++ blog/views.py
@@ -14,6 +14,8 @@ def bloghome(request):
 
 def blogPost(request, slug):
     post = Post.objects.filter(slug=slug).first()
+    post.views = post.views + 1
+    post.save()
     comments = BlogComment.objects.filter(post=post, parent=None)
     replies = BlogComment.objects.filter(post=post).exclude(parent=None)
     repdict = {}
